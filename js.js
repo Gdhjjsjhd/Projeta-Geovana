e7a2a6b8cdb54de8e86c842119b2a91bf35e9557
Gabriel <gabrielpiresmartins2009@gmail.com>
2026-04-13 19:16:07 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,151 @@
+function abrirCard1(nome) {
+    document.getElementById("modal1").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo1").innerText = nome;
+}
+
+function abrirCard2(nome) {
+    document.getElementById("modal2").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo2").innerText = nome;
+}
+
+
+function abrirCard3(nome) {
+    document.getElementById("modal3").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo3").innerText = nome;
+}
+
+
+function abrirCard4(nome) {
+    document.getElementById("modal4").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo4").innerText = nome;
+}
+
+
+function abrirCard5(nome) {
+    document.getElementById("modal5").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo5").innerText = nome;
+}
+
+
+function abrirCard6(nome) {
+    document.getElementById("modal6").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo6").innerText = nome;
+}
+
+
+function abrirCard7(nome) {
+    document.getElementById("modal7").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo7").innerText = nome;
+}
+
+
+function abrirCard8(nome) {
+    document.getElementById("modal8").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo8").innerText = nome;
+}
+
+
+function abrirCard9(nome) {
+    document.getElementById("modal9").style.display = "flex";
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("titulo9").innerText = nome;
+}
+
+function fecharCard1(event) {
+
+    if (event.target.id === "modal1" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal1").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+function fecharCard2(event) {
+
+    if (event.target.id === "modal2" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal2").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+function fecharCard3(event) {
+
+    if (event.target.id === "modal3" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal3").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+function fecharCard4(event) {
+
+    if (event.target.id === "modal4" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal4").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+
+function fecharCard5(event) {
+
+    if (event.target.id === "modal5" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal5").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+
+function fecharCard6(event) {
+
+    if (event.target.id === "modal6" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal6").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+
+function fecharCard7(event) {
+
+    if (event.target.id === "modal7" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal7").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+
+
+function fecharCard8(event) {
+
+    if (event.target.id === "modal8" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal8").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+
+function fecharCard9(event) {
+
+    if (event.target.id === "modal9" || event.target.classList.contains("fechar")) {
+        document.getElementById("modal9").style.display = "none";
+        document.getElementById("container").classList.remove("blur");
+    }
+}
+
+
+
+
+
